8f726b71e31c31fb3d0e05948658310dd532e7fa
artobay <aliyarfatemi@yahoo.com>
2018-08-25 13:24:11 -0700

    Header is completed

---
+++ index.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => {
+  return (
+
+
+   < Header headerText={'Albums'} />
+
+  );
+};
+
+AppRegistry.registerComponent('albums', () => App);
