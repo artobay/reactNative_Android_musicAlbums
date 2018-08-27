e361c206c8cebd3b545f136aadd868ce2184427f
artobay <aliyarfatemi@yahoo.com>
2018-08-26 19:19:48 -0700

    CardSection is Completed and its been used in AlbumDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection'; 
 // AlbumDetail is a presenational component and
 // its using props so it needs to be functional component.
 // we recieved props with name of album and then we go inside that
@@ -8,9 +9,11 @@ import { View, Text } from 'react-native';
 
 const AlbumDetail = (props) => {
  return (
-  <View>
+  <Card>
+  <CardSection>
   <Text>{props.album.title} </Text>
-  </View>
+  </CardSection>
+  </Card>
 
  );
 };
