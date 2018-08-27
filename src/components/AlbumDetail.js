4fdcd5084075cb5a23aa63a93221a3818ffacd3f
artobay <aliyarfatemi@yahoo.com>
2018-08-27 12:32:25 -0700

    Album Covers and Header for each album is completed

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,21 +1,75 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
-import CardSection from './CardSection'; 
+import CardSection from './CardSection';
 // AlbumDetail is a presenational component and
 // its using props so it needs to be functional component.
 // we recieved props with name of album and then we go inside that
 // album and get detail for each album.
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+
+  // using destructuring, means that we are getting these item
+  // out from the album object
+   const { title, artist, thumbnail_image, image } = album;
+
  return (
   <Card>
   <CardSection>
-  <Text>{props.album.title} </Text>
+   <View style={styles.thumbnailContainerStyle}>
+     <Image
+     style={styles.thumbnailStyle}
+     source={{ uri: thumbnail_image }}
+     />
+  </View>
+   <View style={styles.headerContentStyle}>
+  <Text style={styles.headerTextStyle}>{title} </Text>
+  <Text>{artist} </Text>
+  </View>
   </CardSection>
+
+  <CardSection>
+    <Image
+    style={styles.ImageStyle}
+    source={{ uri: image }}
+    />
+  </CardSection>
+
   </Card>
 
  );
+};
+
+const styles = {
+headerContentStyle: {
+  flexDirection: 'column',
+  justifyContent: 'space-around'
+},
+
+headerTextStyle: {
+  fontSize: 18
+},
+
+
+thumbnailStyle: {
+  height: 50,
+  width: 50
+},
+
+thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  ImageStyle: {
+   height: 300,
+   flex: 1,
+    width: null
+  }
+
+
 };
 
 export default AlbumDetail;
--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -5,10 +5,24 @@ import { View } from 'react-native';
 const CardSection = (props) => {
 return (
 
-  <View>
+  <View style={styles.containerStyle}>
     {props.children}
   </View>
 );
+};
+
+const styles = {
+containerStyle: {
+  borderBottomWidth: 1,
+  padding: 5,
+  backgroundColor: '#fff',
+  justifyContent: 'flex-start',
+  flexDirection: 'row',
+  borderColor: '#ddd',
+  position: 'relative'
+}
+
+
 };
 
 export default CardSection;
