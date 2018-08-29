1b1332b1f7e6cd0fd0b58dcdf9d7c0a1cf5aef81
artobay <aliyarfatemi@yahoo.com>
2018-08-28 18:34:56 -0700

    Button Styling is completed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
 
 return (
-   <Text>
-    Click Me !
+  <TouchableOpacity style={styles.buttonStyle}>
+  <Text style={styles.textStyle}>
+       Click Me! 
    </Text>
+  </TouchableOpacity>
 
-);
+ );
 };
 
-export default Button; 
+const styles = {
+    textStyle: {
+       alignSelf: 'center',
+       color: '#007aff',
+       fontSize: 16,
+       fontWeight: '600',
+       paddingTop: 10,
+       paddingBottom: 10
+
+    },
+
+  buttonStyle: {
+     flex: 1,
+     alignSelf: 'stretch',
+     backgroundColor: '#fff',
+     borderRadius: 5,
+     borderWidth: 1,
+     borderColor: '#007aff',
+     marginLeft: 5,
+     marginRight: 5
+  }
+};
+
+
+export default Button;
